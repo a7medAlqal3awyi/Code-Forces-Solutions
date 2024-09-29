2e7f0eeb5935aadbc21580a9820fda36f354e7a1
Ahmed Khaled <a7medalqalawyi@gmail.com>
2024-09-29 18:25:20 +0300

    Say Hello With C++ Done

---
+++ Sheet #1 (Data type - Conditions)/Say Hello With C++.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+
+/*
+* Given a name S. Print "Hello, (name)" without parentheses.
+
+Input
+Only one line containing a string S.
+
+Output
+Print "Hello, " without quotes, then print name.
+
+Example
+InputCopy
+programmer
+OutputCopy
+Hello, programmer
+
+
+ */
+int main()
+{
+    string name;
+    cin>>name;
+    cout << "Hello, " <<name;
+    return 0;
+}
